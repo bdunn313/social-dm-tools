7f8ec416615564096896e7ff33a19e215f8a7952
Brad Dunn <brad@braddunn.com>
2020-03-22 17:31:52 -0400

    Improvements for css adaptability

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const outputDir = path.join(__dirname, "build/");
 const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
@@ -12,5 +13,40 @@ module.exports = {
   output: {
     path: path.join(__dirname, "build"),
     filename: "index.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      inject: false
+    })
+  ],
+  devServer: {
+    compress: true,
+    contentBase: outputDir,
+    port: process.env.PORT || 8000,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css|otf$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
   }
 };
